b4fda34dfec909c196f933b83a3e3e3d882c792a
Arun <youngdeveloper37@gmail.com>
2024-06-06 17:42:07 +0530

    FIX ::  Add BeforeEach method

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -1,31 +1,30 @@
-import { TestBed } from '@angular/core/testing';
-
 import { CalculatorService } from './calculator.service';
-import { LoggerService } from './logger.service';
+// import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
-  const logger = new LoggerService();
-  let service = new CalculatorService(logger);
+  // const logger = new LoggerService();
+
+  let service: CalculatorService, logger: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
+    logger = jasmine.createSpyObj('LoggerService', ['log']);
+    service = new CalculatorService(logger);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should add 2 numbers',()=>{
-    const result = service.add(2,3);
-    expect(result).toBe(5)
-  })
-  it('should subtract 2 numbers',()=>{
-    const result = service.subtract(5,3);
-    expect(result).toBe(2)
-  })
-  it('should multiply 2 numbers',()=>{
-    const result = service.multiply(5,3);
-    expect(result).toBe(15)
-  })
+  it('should add 2 numbers', () => {
+    const result = service.add(2, 3);
+    expect(result).toBe(5);
+  });
+  it('should subtract 2 numbers', () => {
+    const result = service.subtract(5, 3);
+    expect(result).toBe(2);
+  });
+  it('should multiply 2 numbers', () => {
+    const result = service.multiply(5, 3);
+    expect(result).toBe(15);
+  });
 });
